270e1b7369f9f34706cf8f3d08edff6d29778a47
Riffecs <Riffecs@users.noreply.github.com>
2022-07-13 17:27:24 +0000

    deploy: 4f92dd4d2e139a74881d1f38adf6ef20d522d32e

--- search/functions_7.js
+++ search/functions_7.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['info_119',['info',['../classrion_1_1rion_1_1_rion.html#a1346184f3eebb9d422151dc25af803a3',1,'rion::rion::Rion']]],
   ['input_5fvalue_120',['input_value',['../classrion_1_1database_1_1_database.html#a0255760872aa2a3c6a98f4798c329706',1,'rion::database::Database']]],
-  ['install_121',['install',['../classrion_1_1rion_1_1_rion.html#a28187f005e3fc2cdb16208ae63828718',1,'rion::rion::Rion']]],
+  ['install_121',['install',['../classrion_1_1rion_1_1_rion.html#a8aade02eb8015d55b6fc834454f3d59f',1,'rion::rion::Rion']]],
   ['installer_122',['installer',['../classrion_1_1rion_1_1_rion.html#abc05538f283764c7bb8a5cebafd0f51e',1,'rion::rion::Rion']]]
 ];
